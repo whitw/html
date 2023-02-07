62979a7af852f4d1e053edd3cc08da9bdab65572
whitw <shkim7876@naver.com>
2023-02-07 14:48:04 +0900

    rest

--- 7일차/15_spreadAndRest.js
+++ 7일차/15_spreadAndRest.js
@@ -13,3 +13,12 @@ const catTypeAnimals = ['고양이', '호랑이', '사자']
 const dogTypeAnimals = ['개', '늑대', '여우']
 const animals = [...catTypeAnimals, ...dogTypeAnimals, '비버']
 console.log(animals)
+
+const {color, ...another} = colorSquare
+console.log(color)
+console.log(another)
+
+const [first, second, ...rest] = animals
+console.log(first)
+console.log(second)
+console.log(rest)
