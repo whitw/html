88bb4f7222f2a5c21fd7b6e132b634e65fd3025e
whitw <shkim7876@naver.com>
2023-03-03 10:34:47 +0900

    서버-클라이언트 분리 & get/post 테스트

--- NODE/3일차/03/sv/db/index.js
+++ NODE/3일차/03/sv/db/index.js
@@ -1,9 +1,15 @@
 const database = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = database.createPool({
-  host: "",
-  password: "",
-  database: "",
+  // aws ip
+  host: process.env.DB_HOST,
+  // mysql username
+  user: process.env.DB_USER,
+  // mysql user password
+  password: process.env.DB_PASSWORD,
+  // db name
+  database: process.env.DB_SCHEMA,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
